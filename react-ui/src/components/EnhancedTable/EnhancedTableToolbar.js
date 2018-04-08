87eb190ed60fbb62bf709b598667be971e032b5b
TimSusa <timsusa@altus-insight.de>
2018-04-08 11:35:01 +0200

    Limit text in head

--- react-ui/src/components/EnhancedTable/EnhancedTableToolbar.js
+++ react-ui/src/components/EnhancedTable/EnhancedTableToolbar.js
@@ -27,7 +27,7 @@ let EnhancedTableToolbar = (props) => {
             {selectedItems.map((item, i) => {
               return (
                 <Typography key={`${i}-selected`} color='inherit'>
-                  {item && item.name}
+                  {item && item.name.substring(0, 120)}
                 </Typography>
               )
             })}
